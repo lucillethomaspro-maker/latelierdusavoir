14d977c8dfb84ac3870c859e5038e2ea11ac6763
MrLootman <nicolas.michel@wildcodeschool.com>
2026-02-19 16:06:42 +0100

    Insection observer OK

---
+++ scripts/animations.js
@@ -0,0 +1,18 @@
+const methods = document.querySelectorAll(".method");
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("visible");
+      } else {
+        entry.target.classList.remove("visible");
+      }
+    });
+  },
+  { threshold: 0.1 },
+);
+
+methods.forEach((method) => {
+  observer.observe(method);
+});
