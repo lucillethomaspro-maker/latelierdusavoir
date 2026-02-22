b369cbe5c9f7c6c823e2e70b20b572d1f8ab6789
MrLootman <nicolas.michel@wildcodeschool.com>
2026-02-22 20:11:55 +0100

    scroll animation enhanced - mobile version bug fixed

--- docs/scripts/scrollbar.js
+++ docs/scripts/scrollbar.js
@@ -1,3 +1,15 @@
 const main = document.querySelector("main");
-const rect = main.getBoundingClientRect();
-main.style.setProperty("--main-height", `${rect.height}px`);
+
+function updateMainHeight() {
+  console.log("On recalcule, s'il vous plait");
+  main.style.setProperty("--main-height", `${main.offsetHeight}px`);
+}
+
+let resizeTimer;
+
+window.addEventListener("resize", () => {
+  clearTimeout(resizeTimer);
+  resizeTimer = setTimeout(updateMainHeight, 100);
+});
+
+updateMainHeight();
